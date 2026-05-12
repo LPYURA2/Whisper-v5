55f8fe3cc050b99297c00ded792edb4b39ba0691
Yura <vovakrech@gmail.com>
2026-05-12 21:47:59 +0300

    profile-manager.js

--- profile/profile-manager.js
+++ profile/profile-manager.js
@@ -1,3 +1,5 @@
+import { Storage } from '../storage/storage.js';
+
 export const ProfileManager = {
 
     profile: null,
@@ -6,6 +8,66 @@ export const ProfileManager = {
 
         console.log("[Profile] init");
 
+        const savedProfile =
+            await Storage.get("profile");
+
+        if (savedProfile) {
+
+            this.profile = savedProfile;
+
+            console.log(
+                "[Profile] loaded",
+                this.profile
+            );
+
+            return;
+        }
+
+        await this.createProfile();
+    },
+
+    async createProfile() {
+
+        console.log("[Profile] creating");
+
+        const profile = {
+
+            id: crypto.randomUUID(),
+
+            username: "anonymous",
+
+            avatar: "",
+
+            publicKey: "",
+
+            privateKey: "",
+
+            createdAt: Date.now()
+        };
+
+        this.profile = profile;
+
+        await this.saveProfile();
+
+        console.log(
+            "[Profile] created",
+            this.profile
+        );
+    },
+
+    async saveProfile() {
+
+        await Storage.set(
+            "profile",
+            this.profile
+        );
+
+        console.log("[Profile] saved");
+    },
+
+    getProfile() {
+
+        return this.profile;
     }
 
 };
