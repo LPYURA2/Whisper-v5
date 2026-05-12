02ab1b7f9e8d0f4672e280dcd8eaf9b342696988
Yura <vovakrech@gmail.com>
2026-05-12 22:40:52 +0300

    app.js

--- core/app.js
+++ core/app.js
@@ -1,31 +1,39 @@
 import { UI } from '../ui/ui.js';
 import { UILayout } from '../ui/layout.js';
 import { Storage } from '../storage/storage.js';
-import { Signaling } from '../network/signaling.js';
 import { ProfileManager } from '../profile/profile-manager.js';
+import { KeyManager } from '../crypto/key-manager.js';
+import { Signaling } from '../network/signaling.js';
 import { registerSW } from './sw-register.js';
 
 async function bootstrap() {
-  console.log('[Bootstrap] Starting');
 
-  await Storage.init();
+    console.log('[Bootstrap] Starting');
 
-  UI.init();
-  UILayout.init();
+    await Storage.init();
 
-  await registerSW();
+    UI.init();
+    UILayout.init();
 
-  await ProfileManager.init();
+    await registerSW();
 
-  await Signaling.init();
+    await ProfileManager.init();
 
-  console.log('[Bootstrap] Ready');
+    await KeyManager.init();
+
+    await Signaling.init();
+
+    console.log('[Bootstrap] Ready');
 }
 
 bootstrap().catch((err) => {
-  console.error('[Bootstrap] Fatal Error', err);
 
+    console.error(
+        '[Bootstrap] Fatal Error',
+        err
+    );
 });
+
 
 window.addEventListener("ws-message", (event) => {
 
