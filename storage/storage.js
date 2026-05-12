c6d4831d9367a612043936b27079ae0317cdd552
Yura <vovakrech@gmail.com>
2026-05-12 21:52:23 +0300

    storage.js

--- storage/storage.js
+++ storage/storage.js
@@ -1,5 +1,35 @@
 export const Storage = {
-  async init() {
-    console.log('[Storage] initialized');
-  }
+
+    async init() {
+
+        console.log("[Storage] initialized");
+    },
+
+    async get(key) {
+
+        const raw =
+            localStorage.getItem(key);
+
+        if (!raw) {
+            return null;
+        }
+
+        return JSON.parse(raw);
+    },
+
+    async set(key, value) {
+
+        localStorage.setItem(
+            key,
+            JSON.stringify(value)
+        );
+    },
+
+    async remove(key) {
+
+        localStorage.removeItem(key);
+    }
+
 };
+
+window.Storage = Storage;
