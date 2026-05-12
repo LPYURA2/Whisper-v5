06328759869672659aa88d83b9e56f708c4f3c39
Yura <vovakrech@gmail.com>
2026-05-12 22:41:39 +0300

    key-manager.js

--- crypto/key-manager.js
+++ crypto/key-manager.js
@@ -1,9 +1,49 @@
+import { ProfileManager } from '../profile/profile-manager.js';
+
 export const KeyManager = {
+
+    async init() {
+
+        console.log("[KeyManager] init");
+
+        const profile =
+            ProfileManager.getProfile();
+
+        if (
+            profile.publicKey &&
+            profile.privateKey
+        ) {
+
+            console.log(
+                "[KeyManager] keys loaded"
+            );
+
+            return;
+        }
+
+        await this.generateKeyPair();
+    },
 
     async generateKeyPair() {
 
-        console.log("[KeyManager] generating keys");
+        console.log(
+            "[KeyManager] generating keys"
+        );
 
+        const profile =
+            ProfileManager.getProfile();
+
+        profile.publicKey =
+            crypto.randomUUID();
+
+        profile.privateKey =
+            crypto.randomUUID();
+
+        await ProfileManager.saveProfile();
+
+        console.log(
+            "[KeyManager] keys generated"
+        );
     }
 
 };
