46c46394063d1c9ee5556d9079a75f156273d2aa
Yura <vovakrech@gmail.com>
2026-08-12 13:04:33 +0300

    ui.js

--- ui/ui.js
+++ ui/ui.js
@@ -241,6 +241,11 @@ export const UI = {
                 console.log(
                     "[UI] contact added"
                 );
+                /* 
+                * После добавления контакта сразу начинаем попвтеу соединения
+                */
+
+                PeerManager.connect(id);
             }
         );
 
